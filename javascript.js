458b2516de6f59cd875826ab8d46b3b45417a3b9
jorge leonardo fortaleche herrera <jorgefh089@gmail.com>
2022-10-28 22:11:58 -0300

    cambios a mi proyecto

--- javascript.js
+++ javascript.js
@@ -30,24 +30,44 @@ const totalCompra = document.getElementById("totalCompra");
 
 const totalCompraModal = document.getElementById("totalCompraModal");
 
-const numeroDeEntradas = document.getElementById("cantidad de entradas");
+const numeroDeEntradas = document.getElementById("cantidadEntradas");
 
 const productosModal = document.getElementById("productosModal");
 
+const btnFinalizarCompra = document.getElementById("finalizarCompra");
+
+const btnVaciarCarrito = document.getElementById("vaciarCarrito");
+
 let carritoCompras = [];
 
+let arrayFetch;
+
 let total = 0;
+
+let entradaStorage = JSON.parse(localStorage.getItem("carritoCompras"));
+
+if (localStorage.getItem("carritoCompras")) {
+  carritoCompras = entradaStorage;
+  /*  for(let i= 0; i < entradaStorage.length; i++ ){
+    carritoCompras.push(entradaStorage[i]);
+    
+  } */
+  renderizarCarrito();
+  getTotal();
+}
 
 //*Integro FETCH
 
 fetch(listaEntradas)
-  .then((response) =>  response.json())
+  .then((response) => response.json())
   .then((entrada) => {
-     entrada.forEach((element) => {
+    arrayFetch = entrada;
+    console.log(entrada);
+    entrada.forEach((element) => {
       const card = document.createElement("div");
-      card.innerHTML =  `<div>
+      card.innerHTML = `<div>
                              <div class="col d-flex justify-content-center mb-4">
-                                 <div class="card shadow mb-1 bg-white rounded" style="width: 50rem;">
+                                 <div class="card shadow mb-1 bg-white rounded" style="width: 20rem;">
                                    <div class="card-header text-success">
                                       <h5>Disponible</h5>
                                    </div>
@@ -59,19 +79,10 @@ fetch(listaEntradas)
                                 </div>
                              </div>
                         </div> `;
-containerCards.appendChild(card);
+      containerCards.appendChild(card);
 
       let botonCompra = document.getElementById(`botonCompra${element.id}`);
-      //let botonEliminar = document.getElementById(`botonEliminar${element.id}`);
-
       console.log(botonCompra);
-      //console.log(botonEliminar);
-
-      //*Boton para eliminar productos del carrito de compra
-
-     /*  botonEliminar.addEventListener("click", () => {
-        eliminarDeCarrito(element.id);
-      }); */
 
       //*Boton para agregar productos al carrito de compra
 
@@ -84,127 +95,122 @@ containerCards.appendChild(card);
             x: 10,
             y: 30,
           },
-          duration: 2000,
+          duration: 1000,
           stopOnFocus: true,
           style: {
             background: "70, 86, 125",
           },
         }).showToast();
       });
-
-      //*Funcion agregar al carrito de compras
-
-      const agregoAlCarrito = (id) => {
-        
-        const boleta = entrada.find((entrada) => entrada.id === id);
-
-          if(carritoCompras.includes(boleta)){
-            boleta.cantidad ++
-          }else{
-            carritoCompras.push(boleta);
-
-          }
-          //console.log(carritoCompras);
-          productosModal.innerHTML = "";
-            
-         carritoCompras.forEach(element =>{
-        //const div = document.createElement("div");
-        productosModal.innerHTML +=`<p> ${element.nombre}  Cantidad: <span id="cantidadModal">${element.cantidad}</span></P> <button id= "eliminarProductoModal${element.id}" class="btn btn-danger">X</button>`
-
-        //productosModal.appendChild(div);
-
-        })
-        
-
-        
-
-        getTotal()
-
-        //totalCompra.innerText = valorTotal;
-        //numeroDeEntradas.innerHTML = carritoCompras.length;
-
-        //*Agrego producto al storage
-        //addLocalStorage()
-
-      };
-
-      //mostrarCompraStorage()//*LocalStorage
-     
     });
   })
   .catch((error) => console.log(error))
   .finally(() => console.log("Proceso Finalizado"));
 
-  function getTotal(){
+const agregoAlCarrito = (id) => {
+  const boleta = arrayFetch.find((entrada) => entrada.id === id);
 
-    let valorTotal = carritoCompras.reduce((acc, item) => acc + item.precio * item.cantidad,0);
-    totalCompra.innerText = valorTotal;
-    totalCompraModal.innerText = valorTotal; 
-
-  }
-
-//*Funcion eliminar del carrito de compras
-/* const eliminarDeCarrito = (id) => {
-  const boleta = carritoCompras.find((entrada) => entrada.id === id);
-  console.log(boleta);
-  carritoCompras.splice(carritoCompras.indexOf(boleta), 1);
+  carritoCompras.includes(boleta)
+    ? boleta.cantidad++
+    : carritoCompras.push(boleta);
   console.log(carritoCompras);
 
-  const eliminoValor = carritoCompras.reduce((acc, entrada) => acc + entrada.precio,0);
+  //*Agrego producto al storage
+  addLocalStorage();
 
-  console.log(eliminoValor);
+  renderizarCarrito();
 
-  totalCompra.innerText = eliminoValor;
-  numeroDeEntradas.innerText = carritoCompras.length;
+  getTotal();
+};
 
+function getTotal() {
+  let valorTotal = carritoCompras.reduce(
+    (acc, item) => acc + item.precio * item.cantidad,
+    0
+  );
+  let totalCantidades = carritoCompras.reduce(
+    (acc, item) => acc + item.cantidad,
+    0
+  );
 
-  //*Elimino producto al storage
-  addLocalStorage()
-}; */
+  numeroDeEntradas.innerHTML = totalCantidades;
+  totalCompra.innerText = valorTotal;
+  totalCompraModal.innerText = valorTotal;
+}
 
+//*Funcion mostrar productos en ventana modal
+function renderizarCarrito() {
+  productosModal.innerHTML = "";
+  carritoCompras.forEach((element) => {
+    productosModal.innerHTML += `
+    <div class="container">
+     <div class="row">
+       <div class="col">
+         <p> ${element.nombre} </p>
+       </div>
+      <div class="col-sm-auto">
+        <span id="cantidadModal">${element.cantidad}</span>
+      </div>
+      <div class="col col-sm-2">
+        <button class="btn btn-danger" onclick="eliminoProd(${element.id})">X</button>
+      </div>
+    </div>`; //*Llamado a la funcion eliminarProducto
+  });
+  productosModal.innerHTML;
+}
 
+//*Funcion eliminar productos
+function eliminoProd(id) {
+  let boleta = carritoCompras.find((item) => item.id === id);
+  boleta.cantidad > 1
+    ? boleta.cantidad--
+    : carritoCompras.splice(carritoCompras.indexOf(boleta), 1);
+  addLocalStorage();
+
+  renderizarCarrito();
+
+  getTotal();
+
+  console.log(carritoCompras);
+}
 
 //*Funciones JSON y localStorage
 
-function addLocalStorage(){
-    localStorage.setItem("carritoCompras", JSON.stringify(carritoCompras))
-    
+function addLocalStorage() {
+  localStorage.setItem("carritoCompras", JSON.stringify(carritoCompras));
 }
 
+//*Finalizo la compra
+btnFinalizarCompra.addEventListener("click", () => {
+  Swal.fire({
+    icon: "question",
+    iconColor: "#fcf300",
+    title: "Estas a un paso de vivir tu proximo evento",
+    text: `Quieres continuar con tu compra?`,
+    confirmButtonText: "Aceptar",
+    showCancelButton: true,
+    cancelButtonText: "Cancelar",
+    cancelButtonColor: "#bf0603",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      carritoCompras.splice(0, carritoCompras.length);
+      renderizarCarrito();
+      getTotal();
+      addLocalStorage();
+      console.log(carritoCompras);
+      Swal.fire({
+        icon: "success",
+        title: "Gracias por elegirnos disfruta de tu evento",
+        backdrop: "#000000",
+      });
+    }
+  });
+});
 
-/* function mostrarCompraStorage () {
-
-  if(localStorage.getItem("carritoCompras")){
-      let entradaStorage = JSON.parse(localStorage.getItem("carritoCompras"));
-      carritoCompras = entradaStorage
-  
-      let resultado = entradaStorage.reduce((acc, entrada) => acc + entrada.precio,0);
-  
-      numeroDeEntradas.innerText = entradaStorage.length
-      totalCompra.innerText = resultado
-      
-  }
-
-} */
-
-
-
-const eliminarProductoModal = (id) =>{
-  
-  const boleta = carritoCompras.find((entrada) => entrada.id === id);
-  console.log(boleta);
-  carritoCompras.splice(carritoCompras.indexOf(boleta), 1);
-  console.log(carritoCompras);
-
-  console.log("Hiciste click");
-
-  agregarCompraModal()
-
-
- }
-
-           
-
-
-    
-
+btnVaciarCarrito.addEventListener("click", ()=>{
+  carritoCompras.splice(0, carritoCompras.length);
+    renderizarCarrito();
+    getTotal();
+    addLocalStorage();
+    console.log(carritoCompras);
+})
